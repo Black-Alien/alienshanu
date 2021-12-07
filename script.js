074c3fc39e1c28148330742c2901b5e4f2a47a60
Black Alien <52879751+Black-Alien@users.noreply.github.com>
2021-12-07 19:12:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const COMMANDS = {
   help:
     'Supported commands: <span class="code">about</span>, <span class="code">experience</span>, <span class="code">education</span>, <span class="code">skills</span>, <span class="code">acknowledges</span>, <span class="code">certificates</span>, <span class="code">contact</span>',
   about:
-    "Hello 👋<br>I'm Akash Sebastian. Information Security enthusiast, I do bug bounty hunting, Capture The Flag as a hobby and passion",
+    "Hello 👋<br>I'm Alien Shanu. Information Security enthusiast, I do bug bounty hunting, Capture The Flag as a hobby and passion",
   skills:
     '<span class="code">Skill:</span>Pentesting, Programming, Bughunting...<br>',
   education:
@@ -25,7 +25,7 @@ acknowledges:
     "<a href='https://bughunter.withgoogle.com/profile/c199d616-6cc3-4e90-a3e2-31e80f0ec1a2' class='success link'>Google Hall of Fame</a> , <a href='https://www.nokia.com/responsible-disclosure/' class='success link'>Nokia HOF</a> , <a href='https://dev.to/security' class='success link'>dev.to HOF</a> , <a href='https://bugcrowd.com/netflix/hall-of-fame' class='success link'>Netflix HOF</a> , <a href='https://bugcrowd.com/pinterest/hall-of-fame' class='success link'>Pinterest HOF</a> , <a href='https://www.philips.com/a-w/security/coordinated-vulnerability-disclosure/hall-of-honors.html' class='success link'>Philips HOF</a> , <a href='https://bugcrowd.com/soundcloud/hall-of-fame' class='success link'>Soundcloud HOF</a> , <a class='success link'>many more...</a>  ", 
   contact:
     "You can contact me on any of following :<br><a href='https://t.me/akashsebastian' class='success link'>Telegram</a>, <a href='https://www.instagram.com/akashsebastian333/' class='success link'>Instagram</a>, <a href='https://www.twitter.com/akashseb/' class='success link'>Twitter</a>, <a href='mailto:mail@akashsebastian.com' class='success link'>Email</a>, <a href='https://api.whatsapp.com/send?phone=+919746626938' class='success link'>whatsapp</a>",
-  alien: "<iframe width="1350" height="480" src="https://www.youtube.com/embed/sQjeYproNfU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"
+  alien: "<iframe width='1350' height='480' src='https://www.youtube.com/embed/sQjeYproNfU' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
 
 };
 
