e7e70ac887c1b76517393a584c9234e04e574ead
Black Alien <52879751+Black-Alien@users.noreply.github.com>
2021-12-08 03:05:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const COMMANDS = {
   
   hackbar:'Supported commands: <span class="code">download</span>, <span class="code">installation</span>',
   download:"<a href='https://download1653.mediafire.com/uvqewa3d3qhg/n2ohtkdpv33gxh5/HBM+Hackbar+2020-v.07.1.xpi' class='success link'>click here to download</a>",
-  installation:'<iframe width="300" height="250" src="https://www.youtube.com/embed/j58ETtS0avw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+  installation:'Supported commands: <iframe width="300" height="250" src="https://www.youtube.com/embed/j58ETtS0avw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
   
   
   
